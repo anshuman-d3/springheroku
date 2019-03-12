b5151ae66ad4a359681e956a950ff000170ea3c3
anshuman-d3 <anshuman.d3@gmail.com>
2019-03-12 16:51:42 +0530

    Role ID field modified for Postgresql.

--- src/main/java/com/springapp/springuser/models/UserRole.java
+++ src/main/java/com/springapp/springuser/models/UserRole.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class UserRole {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
     private String name;
